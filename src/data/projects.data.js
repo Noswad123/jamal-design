86911c628c089ee09981d9dd0478efe5fc160bdb
Jamal Dawson <jamal.a.dawson@gmail.com>
2018-04-03 19:24:45 -0400

    new link

--- src/data/projects.data.js
+++ src/data/projects.data.js
@@ -1,45 +1,37 @@
-const Projects=[
-    {
-        name:"Breaking Chains",
-        description:"An app that can be used it keep track of a game of disc golf.  It is build with Angular, Express, and MongoDB",
-        url:"https://github.com/Noswad123/breaking-chains",
-        tech:["./img/html.png","./img/sass.png","./img/angular.svg", "./img/mongo.png", "./img/express.png"],
-        imgUrl:"./img/breakin_chains.png"
+const Projects = [{
+        name: "Breaking Chains",
+        description: "An app that can be used it keep track of a game of disc golf. Users are also able to manage their collection of discs.  It is build with Angular, Express, and MongoDB",
+        url: "https://breaking-chains.herokuapp.com/login",
+        tech: ["./img/html.png", "./img/sass.png", "./img/angular.svg", "./img/mongo.png", "./img/express.png"],
+        imgUrl: "./img/breakin_chains.png"
     },
     {
-        name:"Cartoon Cars",
-        description:"First react app, so I'm having fun experimenting with its features.  In this app, You may view a gallery of fantasy cars.",
-        url:"http://cartoon-car.herokuapp.com/",
-        tech:["./img/html.png","./img/css.png","./img/react.ico", "./img/heroku.svg"],
-        imgUrl:"./img/speedracer.jpg"
+        name: "Cartoon Cars",
+        description: "First react app, so I'm having fun experimenting with its features.  In this app, You may view a gallery of fantasy cars.",
+        url: "http://cartoon-car.herokuapp.com/",
+        tech: ["./img/html.png", "./img/css.png", "./img/react.ico", "./img/heroku.svg"],
+        imgUrl: "./img/speedracer.jpg"
     },
     {
-        name:"Jamal design catalogue",
-        description:"Where I am keeping all of my resusable code. It demonstrates various layouts and stylings.",
-        url:"https://github.com/Noswad123/web-design-catalogue",
-        tech:["./img/html.png","./img/css.png","./img/react.ico"],
-        imgUrl:"./img/jml.png"
+        name: "Jamal design catalogue",
+        description: "Where I am keeping all of my resusable code. It demonstrates various layouts and stylings.",
+        url: "https://jamal-design-catalogue.herokuapp.com/",
+        tech: ["./img/html.png", "./img/css.png", "./img/react.ico"],
+        imgUrl: "./img/jml.png"
     },
     {
-        name:"Original Furnishings",
-        description:"An app I am building for a dear friend to show off his custom built furniture.",
-        url:"http://original-furnishings.herokuapp.com/",
-        tech:["./img/html.png","./img/css.png","./img/react.ico","./img/heroku.svg"],
-        imgUrl:"./img/bench.png"
+        name: "Original Furnishings",
+        description: "An app I am building for a dear friend to show off his custom built furniture.",
+        url: "http://original-furnishings.herokuapp.com/",
+        tech: ["./img/html.png", "./img/css.png", "./img/react.ico", "./img/heroku.svg"],
+        imgUrl: "./img/bench.png"
     },
     {
-        name:"SWAG to College",
-        description:"Goal of this project is to migrate the existing site www.swagtocollege.org from weebly to a reactjs app.  At a click of a button, visitors to the site may change between English and Spanish",
-        url:"http://swag-to-college.herokuapp.com",
-        tech:["./img/html.png","./img/css.png","./img/react.ico", "./img/heroku.svg"],
-        imgUrl:"./img/swag.png"
-    },
-    {
-        name:"Task Bounty",
-        description:"An help that will help communities fix problems in their neighborhood.  It uses Angular and MongoDb.",
-        url:"https://github.com/Noswad123/taskbounty",
-        tech:["./img/html.png","./img/sass.png","./img/angular.svg"],
-        imgUrl:"./img/taskbounty.jpg"
+        name: "SWAG to College",
+        description: "Goal of this project is to migrate the existing site www.swagtocollege.org from weebly to a reactjs app.  At a click of a button, visitors to the site may change between English and Spanish",
+        url: "http://swag-to-college.herokuapp.com",
+        tech: ["./img/html.png", "./img/css.png", "./img/react.ico", "./img/heroku.svg"],
+        imgUrl: "./img/swag.png"
     }
 ]
 export default Projects;
